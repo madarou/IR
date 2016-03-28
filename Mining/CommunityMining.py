f5f9dee485306eb96d16a353f4a657be5db7ab10
makao <2016333261@qq.com>
2016-03-28 20:22:43 +0800

    basic propagation implemented

--- Mining/CommunityMining.py
+++ Mining/CommunityMining.py
@@ -10,6 +10,9 @@ Created on 2016年3月26日
 通过标签传播方式挖掘用户的兴趣社区
 先不考虑使用rough core，因为所有标签都有初始化过了
 '''
+def isnumeric(s):
+    '''returns True if string s is numeric'''
+    return all(c in "0123456789.+-" for c in s)
 
 def user_propagate(x, source_list, target_list, user_relation_list):
     '''
@@ -24,23 +27,60 @@ def user_propagate(x, source_list, target_list, user_relation_list):
     x_user_relation = user_relation_list[x]
     x_neighbors_list = x_user_relation.follower
     for (key, value) in x_interest_dict.items():
-        neighbor_counter = 0
+        #如果是非兴趣标签，就不传播
+        if not isnumeric(key):
+            continue
+        #neighbor_counter = 0
         for item in x_neighbors_list:
             #item是x的邻居的用户编号，字符型
             neighbor_user_intr = source_list[int(item)]
             neighbor_interest_dict = neighbor_user_intr.interests
             #如果neighbor_user_intr有标签key
             if key in neighbor_interest_dict.keys():
-                #如果是非兴趣标签，就不传播
                 #通过之前设计的公式计算当前标签key，用户x和当前邻居neighbor之间的传播衰减系数w(weaken)
                 w = computeWeaken(key, x, int(item)) 
-                target_list[neighbor_counter].interests[key]=neighbor_interest_dict[key] + value*w
-            neighbor_counter = neighbor_counter + 1
+                target_list[int(item)].interests[key]=neighbor_interest_dict[key] + value*w
+            else:
+                w = computeWeaken(key, x, int(item))
+                target_list[int(item)].interests[key]=value*w
+            #neighbor_counter = neighbor_counter + 1
 
+def normalizeIntr(before_list, after_list, user_interest_list, I):
+    '''
+    每轮传播结束后，整理各个用户的兴趣度，包括
+    1.选出兴趣度最高的I个兴趣和兴趣度
+    2.重新计算用户兴趣度
+    '''
+    for i in range(0,len(before_list)):
+        #对user的interest_dict按value值从大到小进行排序
+        user = before_list[i]
+        interest_dict = user.interests
+        #interest_dict.sort(lambda y, x: cmp(x[1],y[1]))
+        sorted(interest_dict.items(), key = lambda d:d[1])
+        counter = 0 #记录取最大的前I个兴趣
+        
+        user_after = User_Intr(user_interest_list[i].name)
+        new_interest_dict = {}
+        #选出最大的I个兴趣
+        intr_sum = 0#前I个兴趣的度的总和，后面归一化用
+        for (key, value) in interest_dict.items():
+            if counter == I:
+                break
+            new_interest_dict[key] = value
+            intr_sum = intr_sum + string.atof(value)
+            counter = counter + 1
+        #归一化
+        for (key, value) in new_interest_dict.items():
+            new_interest_dict[key] = string.atof(value)/intr_sum
+
+        user_after.setInterest(new_interest_dict)
+        after_list.append(user_after)
+        
 def computeWeaken(tag, user_i, user_j):
     return 1
 
 if __name__ == '__main__':
+    MAX_TAG_NUM = 8#传播时，每个用户保留的最大标签数
     user_interest_list=[]
     line_counter = 0
     for line in open('/Users/makao/Yun/Workspace/lab/data/num_user_intr.txt'):
@@ -74,9 +114,13 @@ if __name__ == '__main__':
     for i in range(0, line_counter):
         user = User_Intr('None')
         target_list.append(user)
+    
+    after_list = []
         
     for i in range(0, line_counter):
         user_propagate(i, user_interest_list, target_list, user_relation_list)
         
-    for item in target_list:
+    normalizeIntr(target_list, after_list, user_interest_list, MAX_TAG_NUM)
+        
+    for item in after_list:
         print item
