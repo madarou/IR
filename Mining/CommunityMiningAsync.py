a7fbf8e5c6edb6224dd8cc9e6b0e8b1ee1d65739
makao <2016333261@qq.com>
2016-03-30 13:55:48 +0800

    finish asynchronized propagation, works well

--- Mining/CommunityMiningAsync.py
+++ Mining/CommunityMiningAsync.py
@@ -4,26 +4,25 @@ from model.User_Intr import User_Intr
 from model.User_Relation import User_Relation
 import string
 '''
-Created on 2016年3月26日
+Created on 2016年3月30日
 
 @author: makao
 通过标签传播方式挖掘用户的兴趣社区
 先不考虑使用rough core，因为所有标签都有初始化过了
 
-3月30日改：
-同步更新的方式，结果不收敛
+异步更新的方式
 '''
 def isnumeric(s):
     '''returns True if string s is numeric'''
     return all(c in "0123456789.+-" for c in s)
 
-def user_propagate(x, source_list, target_list, user_relation_list):
+def user_propagate(x, source_list, user_relation_list):
     '''
     用户x传播，去影响他的邻居，int型
     source_list是当前用户兴趣度列表
     target_list是用户x传播后的，初始时为0
     
-    同步更新的方式
+    异步更新的方式
     '''
     x_user_intr = source_list[x]
     x_interest_dict = x_user_intr.interests
@@ -42,18 +41,19 @@ def user_propagate(x, source_list, target_list, user_relation_list):
             if key in neighbor_interest_dict.keys():
                 #通过之前设计的公式计算当前标签key，用户x和当前邻居neighbor之间的传播衰减系数w(weaken)
                 w = computeWeaken(key, x, int(item)) 
-                target_list[int(item)].interests[key]=neighbor_interest_dict[key] + value*w
+                source_list[int(item)].interests[key]=neighbor_interest_dict[key] + value*w
             else:
                 w = computeWeaken(key, x, int(item))
-                target_list[int(item)].interests[key]=value*w
+                source_list[int(item)].interests[key]=value*w
             #neighbor_counter = neighbor_counter + 1
 
-def normalizeIntr(before_list, after_list, user_interest_list, I):
+def normalizeIntr(before_list, user_interest_list, I):
     '''
     每轮传播结束后，整理各个用户的兴趣度，包括
     1.选出兴趣度最高的I个兴趣和兴趣度
     2.重新计算用户兴趣度
     '''
+    after_list = []
     for i in range(0,len(before_list)):
         #对user的interest_dict按value值从大到小进行排序
         user = before_list[i]
@@ -67,6 +67,9 @@ def normalizeIntr(before_list, after_list, user_interest_list, I):
         #选出最大的I个兴趣
         intr_sum = 0#前I个兴趣的度的总和，后面归一化用
         for (key, value) in interest_dict.items():
+            #如果是非兴趣标签，就不传播
+            if not isnumeric(key):
+                continue
             if counter == I:
                 break
             new_interest_dict[key] = value
@@ -74,10 +77,15 @@ def normalizeIntr(before_list, after_list, user_interest_list, I):
             counter = counter + 1
         #归一化
         for (key, value) in new_interest_dict.items():
-            new_interest_dict[key] = string.atof(value)/intr_sum
+            if intr_sum > 0:
+                new_interest_dict[key] = string.atof(value)/intr_sum
+            else:
+                new_interest_dict[key] = 0
 
         user_after.setInterest(new_interest_dict)
         after_list.append(user_after)
+        
+    return after_list
         
 def computeWeaken(tag, user_i, user_j):
     return 1
@@ -131,33 +139,31 @@ if __name__ == '__main__':
 #     for line in open('/Users/makao/Yun/Workspace/lab/data/rough_core.txt'):
 #         cores_list.append(line.strip().strip('[]').split(', '))
     #初始化
-    target_list = []
+    source_list = user_interest_list[:]
+        
     for i in range(0, line_counter):
-        user = User_Intr('None')
-        target_list.append(user)
+        user_propagate(i, source_list, user_relation_list)
+        
+    normalized_list = normalizeIntr(source_list, user_interest_list, MAX_TAG_NUM)
+    del source_list[:]
+    source_list = normalized_list[:]
     
-    after_list = []
-        
-    for i in range(0, line_counter):
-        user_propagate(i, user_interest_list, target_list, user_relation_list)
-        
-    normalizeIntr(target_list, after_list, user_interest_list, MAX_TAG_NUM)
-    old_tag_distribution = tag_distribution(after_list)
+    old_tag_distribution = tag_distribution(source_list)
     new_tag_distribution = set()
     
     #如果没有稳定，需要再次传播，after_list就成了下一轮的初始传播list
     run = 0
     while len(old_tag_distribution.symmetric_difference(new_tag_distribution))>0:
         print str(run) + ' run'
-        for item in target_list:
-            item.setInterest({})
-        old_tag_distribution = tag_distribution(after_list)
+        old_tag_distribution = tag_distribution(source_list)
         for i in range(0, line_counter):
-            user_propagate(i, after_list, target_list, user_relation_list)
-        del after_list[:]
-        normalizeIntr(target_list, after_list, user_interest_list, MAX_TAG_NUM)
-        new_tag_distribution = tag_distribution(after_list)
+            user_propagate(i, source_list, user_relation_list)
+            
+        normalized_list = normalizeIntr(source_list, user_interest_list, MAX_TAG_NUM)
+        del source_list[:]
+        source_list = normalized_list[:]
+        new_tag_distribution = tag_distribution(source_list)
         run = run + 1
     
-    for item in after_list:
+    for item in source_list:
         print item
