9f9fad78a6e470ec7d21e45470e5dbd30fb10228
makao <2016333261@qq.com>
2016-03-30 12:19:08 +0800

    implemented synchronize method, not converge

--- Mining/CommunityMining.py
+++ Mining/CommunityMining.py
@@ -79,6 +79,24 @@ def normalizeIntr(before_list, after_list, user_interest_list, I):
 def computeWeaken(tag, user_i, user_j):
     return 1
 
+def tag_distribution(user_interest_list):
+    '''
+    统计user_interest_list中各个兴趣总共出现的次数，即各个tag出现的user个数
+    '''
+    result = set()
+    result_dict = {}
+    for item in user_interest_list:
+        interests_dict = item.interests
+        for key in interests_dict.keys():
+            if key in result_dict.keys():
+                result_dict[key] = result_dict[key] + 1
+            else:
+                result_dict[key] = 1
+    for (key, value) in result_dict.items():
+        result.add((key, value))
+    
+    return result
+
 if __name__ == '__main__':
     MAX_TAG_NUM = 8#传播时，每个用户保留的最大标签数
     user_interest_list=[]
@@ -121,6 +139,22 @@ if __name__ == '__main__':
         user_propagate(i, user_interest_list, target_list, user_relation_list)
         
     normalizeIntr(target_list, after_list, user_interest_list, MAX_TAG_NUM)
-        
+    old_tag_distribution = tag_distribution(after_list)
+    new_tag_distribution = set()
+    
+    #如果没有稳定，需要再次传播，after_list就成了下一轮的初始传播list
+    run = 0
+    while len(old_tag_distribution.symmetric_difference(new_tag_distribution))>0:
+        print str(run) + ' run'
+        for item in target_list:
+            item.setInterest({})
+        old_tag_distribution = tag_distribution(after_list)
+        for i in range(0, line_counter):
+            user_propagate(i, after_list, target_list, user_relation_list)
+        del after_list[:]
+        normalizeIntr(target_list, after_list, user_interest_list, MAX_TAG_NUM)
+        new_tag_distribution = tag_distribution(after_list)
+        run = run + 1
+    
     for item in after_list:
         print item
